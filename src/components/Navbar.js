9f9e65278ddac10632d6b5e34b9525f3422a19f2
berke <bab26@st-andrews.ac.uk>
2022-06-19 13:47:19 +0300

    Added styling for the navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ export default function Navbar() {
   return (
     <div className="navbar">
         <nav>
-            <Link to="/">
+            <Link to="/" className='brand'>
                 <h1>Cooking Ninja</h1>
             </Link>
             <Link to="/create">
