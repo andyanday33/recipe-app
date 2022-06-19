2a08b2b1834e3615e7378eb1f684f58625ceb7a4
berke <bab26@st-andrews.ac.uk>
2022-06-19 13:41:50 +0300

    Added a simple navbar component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './pages/home/Home'
 import Create from './pages/create/Create'
 import Recipe from './pages/recipe/Recipe'
 import Search from './pages/search/Search'
+import Navbar from './components/Navbar'
 
 // styles
 import './App.css'
@@ -13,6 +14,7 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/create" element={<Create />} />
---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom'
+
+// styles
+import './Navbar.css'
+
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+        <nav>
+            <Link to="/">
+                <h1>Cooking Ninja</h1>
+            </Link>
+            <Link to="/create">
+                Create Recipe
+            </Link>
+        </nav>
+    </div>
+  )
+}
